e98f377fa0fd1fc64353dc00dd3a3a626d42b42d
Catherine Bronzo <cbronzo@mac.com>
2018-07-05 02:39:16 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -12,9 +12,8 @@ class Author
   end
   
   def add_post(post)
-    @@posts << title
-    title.author = self
-    @@post_count += 1
+    @@posts << post
+    post.author = self
   end
   
   def add_post_by_title(title)
