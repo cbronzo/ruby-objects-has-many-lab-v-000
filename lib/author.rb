a9efbc581ec63bf673a684f4ec14ab9e095a832c
Catherine Bronzo <cbronzo@mac.com>
2018-07-05 02:32:34 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,34 @@
+class Author
+  attr_accessor :name
+  
+  @@post_count = 0
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def posts
+    @posts
+  end
+  
+  def add_post(title)
+    self.posts << title
+    title.author = self
+    @@post_count += 1
+  end
+  
+  def add_post_by_title(title)
+    title = Post.new(name)
+    self.posts << title
+    title.author = self
+    @@post_count += 1
+  end
+  
+  def self.post_count
+    @@post_count
+  end
+    
+end
+
+ 
