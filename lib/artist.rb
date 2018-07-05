41b7a3fb0d12797c3fe802493a7d6d7ed6d246d3
Catherine Bronzo <cbronzo@mac.com>
2018-07-05 02:36:55 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,10 +21,7 @@ class Artist
   end
   
   def add_song_by_name(name)
-    song = Song.new(name)
-    self.songs << song
-    song.artist = self
-    @@song_count +=1
+    
   end
   
   def self.song_count
